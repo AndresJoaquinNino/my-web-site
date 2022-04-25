ad2c6f9c84611719f3868c3d756d010c34209ca8
AndresJoaquinNino <andresninorodil@gmail.com>
2022-04-25 19:47:15 -0400

    adding scroll animations

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,39 @@
 import './Navbar.scss';
-import { motion } from 'framer-motion';
+import { motion, useViewportScroll, useTransform } from 'framer-motion';
 import { MdOutlineMenu, MdOutlineClose } from "react-icons/md";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
 const Navbar = () => {
     const [isOpenSidebar, setIsOpenSidebar] = useState(false);
+    const { scrollYProgress } = useViewportScroll();
+    const defaultNavStatus = scrollYProgress.current < 0.01 ? 'top' : 'up';
+    const [navbarStatus, setNavbarStatus] = useState(defaultNavStatus);
 
+    useEffect(() => {
+        return scrollYProgress.onChange(() => {
+            const { current, prev } = scrollYProgress;
+            if(current < 0.01) return setNavbarStatus('top')
+            if(current < prev) setNavbarStatus('up')
+            else setNavbarStatus('down')
+        })
+    });
+
+    const navbarAnimations = {
+        'down':{
+            y : '-4rem',
+        },
+        'up':{
+            backdropFilter:'blur(15px)',
+            backgroundColor:'#42424266'
+        },
+        'top':{
+            backgroundColor:'#42424200'
+        }
+    }
     return(
         <header className='page-header'>
-            <nav className='page__navbar'>
+            <motion.nav className='page__navbar' animate={navbarAnimations[navbarStatus]}
+            transition={{type:'tween'}}>
                 <div className='page__navbar-lang'>
                     EN
                 </div>
@@ -20,7 +46,7 @@ const Navbar = () => {
                 <div className="page__navbar-icon" onClick={() => setIsOpenSidebar(!isOpenSidebar)}>
                     <MdOutlineMenu/>
                 </div>
-            </nav>
+            </motion.nav>
             <motion.aside className="page__sidebar" animate={{width: isOpenSidebar ? '17.5rem' : '0vw'}}>
                 <div className="page__sidebar-icon" onClick={() => setIsOpenSidebar(!isOpenSidebar)}>
                     <MdOutlineClose/>
