d92425ffc43fc9b00816bdb2102b3e3fc2662fb0
AndresJoaquinNino <andresninorodil@gmail.com>
2022-04-28 12:19:18 -0400

    Adding not foun page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.scss';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
 const App = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home/>}/>
-                <Route path="*" element={<h1>404</h1>}/>
+                <Route path="*" element={<NotFound/>}/>
             </Routes>
         </BrowserRouter>
     );
