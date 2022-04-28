dda36e88f56ff513e4cafd923a50f24504eb1355
AndresJoaquinNino <andresninorodil@gmail.com>
2022-04-28 12:19:04 -0400

    Created the 404 page

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,22 @@
+import './NotFound.scss';
+import { Link } from 'react-router-dom';
+
+const NotFound = () => {
+    return(
+        <div className='wrapper'>
+            <div className='page'>
+                <div className="page__not-found">
+                    <header className='page__not-found-header'>
+                        <h1 className='gradient-text'>
+                            404 / Not Found
+                        </h1>
+                        <hr className="divider-primary mb-2" />
+                    </header>
+                    <Link className='btn btn--secondary-transp' to='/'>My Resume</Link>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default NotFound;
