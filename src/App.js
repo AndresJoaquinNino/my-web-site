fb2444a985766e8bba51e3d5c19ed0a5e2b0de0a
AndresJoaquinNino <andresninorodil@gmail.com>
2022-04-28 21:09:32 -0400

    Setting up the i18next for multi language

--- src/App.js
+++ src/App.js
@@ -2,15 +2,36 @@ import './App.scss';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
+import { I18nextProvider } from 'react-i18next';
+import i18next from 'i18next';
+import spanish from './translations/spanish.json';
+import english from './translations/english.json';
+
+i18next.init({
+    interpolation: {
+        escapeValue: false
+    },
+    lng: 'en',
+    resources:{
+        es: {
+            global: spanish
+        },
+        en: {
+            global: english
+        }
+    }
+})
 
 const App = () => {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="*" element={<NotFound/>}/>
-            </Routes>
-        </BrowserRouter>
+        <I18nextProvider i18n={i18next}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="*" element={<NotFound/>}/>
+                </Routes>
+            </BrowserRouter>
+        </I18nextProvider>
     );
 }
 export default App;
