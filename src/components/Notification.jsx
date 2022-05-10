3b2b27c8ae8b7739bed496e2fbc073247caafd03
AndresJoaquinNino <andresninorodil@gmail.com>
2022-05-10 08:04:15 -0400

    adjusting border

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -5,10 +5,11 @@ import { MdClose } from 'react-icons/md'
 const Notification = ({ type, message, close, index }) => {
     return(
         <AnimatePresence>
-            <motion.div className={`notification ${type === 'error' ? 'notification--error' : ''}`}
+            <motion.div className='notification'
             initial={{ opacity: 0, y: 50, scale: 0.3 }}
             animate={{ opacity: 1, y: 0, scale: 1 }}
             exit={{ opacity: 0, scale: 0.5, transition: { duration: 0.2 } }}>
+                <div className={`notification-bar ${type === 'error' ? 'notification-bar--error' : ''}`}/>
                 <div className='notification-message'>
                     { message }
                 </div>
